96686bbeb9d48cdda8a327470ea00b85f6ccaa4b
KateParshina <kateparshina666@gmail.com>
2017-05-14 14:11:59 +0300

    config

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=planner',
+    'username' => 'stud',
+    'password' => '1111',
+    'charset' => 'utf8',
+];
